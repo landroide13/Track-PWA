53c01957d67d2833458f1d6a6edd559d930c5492
Luis Andrade <landroide@Luiss-MacBook-Pro.local>
2023-05-16 12:11:24 +1200

    tracking loc

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,17 @@
 const Lat = document.getElementById('Lat');
 const Lon = document.getElementById('Lon');
 
+const traLat = document.getElementById('traLat');
+const traLon = document.getElementById('traLon'); 
+
 const showPosition = (position) => {
     Lat.innerHTML = position.coords.latitude;
     Lon.innerHTML = position.coords.longitude;
+}
+
+const trackPosition = (position) => {
+    traLat.innerHTML = position.coords.latitude;
+    traLon.innerHTML = position.coords.longitude;
 }
 
 const getLocation = () => {
@@ -15,6 +23,16 @@ const getLocation = () => {
         Lat.innerHTML = "Geolocation is not supported by this browser.";
     }
 };
+
+const trackLocation = () =>{
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(trackPosition);
+      } else {
+        Lat.innerHTML = "Geolocation is not supported by this browser.";
+      }
+}
+
+trackLocation();
 
 getLocation();
 
